6274c98811652523ceb9c0ff0a455fc00930ccbd
Mateusz <mateusz.gorski12345@wp.pl>
2023-06-06 09:45:13 +0200

    refactor

--- init.sql
+++ init.sql
@@ -1,10 +1,9 @@
-DELETE TABLE IF EXISTS public."Books"
-CREATE TABLE public."Books"
+CREATE TABLE IF NOT EXISTS public."Books"
 (
     book_id integer NOT NULL,
     bookname character varying NOT NULL,
     author character varying NOT NULL,
-    price integer NOT NULL,
+    price integer NOT NULL
 );
 ALTER TABLE IF EXISTS public."Books"    
    OWNER to postgres;
