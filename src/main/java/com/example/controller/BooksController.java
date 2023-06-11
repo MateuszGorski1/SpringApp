a4a669671fb81c328930f2494fec3cc18a5097c7
MateuszGorski1 <mateusz.gorski12345@wp.pl>
2023-06-11 20:07:35 +0200

    test

--- src/main/java/com/example/controller/BooksController.java
+++ src/main/java/com/example/controller/BooksController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "test.spring")
 public class BooksController {
 
     @Autowired
